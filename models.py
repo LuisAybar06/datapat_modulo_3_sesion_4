84c6b6a6180898c37f938f7677e291b1544a16dc
PC <aluisaybar@gmail.com>
2024-12-05 20:48:32 -0500

    first update 5

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    file_name = Column(String, nullable=False)
+    prediction = Column(Float, nullable=False)
+    create_at = Column(TIMESTAMP, nullable=False)
