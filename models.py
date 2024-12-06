6464f1913c049fde8399b6d51b01edbe752ad9d2
PC <aluisaybar@gmail.com>
2024-12-05 21:24:07 -0500

    first update 14

--- models.py
+++ models.py
@@ -3,11 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class Prediction(Base):
     __tablename__ = "predictions"
 
     id = Column(Integer, primary_key=True, index=True)
     file_name = Column(String, nullable=False)
     prediction = Column(Float, nullable=False)
-    create_at = Column(TIMESTAMP, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False) 
